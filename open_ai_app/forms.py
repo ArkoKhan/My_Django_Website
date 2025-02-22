e70884f1d5cfd956051fea18f23ab3bf20387760
ArkoKhan <hasin.arko@gmail.com>
2025-02-23 04:53:59 +0600

    deleted some files

--- open_ai_app/forms.py
+++ open_ai_app/forms.py
@@ -6,3 +6,21 @@ class InputForm(forms.Form):
         max_length=1500,
         widget=forms.TextInput(attrs={ "class": "form-control" })
     )
+
+
+
+class DeepAiInputForm(forms.Form):
+    CHOICES = {
+        ('chat', 'Chat'),
+        ('image', 'Image')
+    }
+    model = forms.ChoiceField(
+        label='Model',
+        choices=CHOICES,
+        widget=forms.Select(attrs={ "class": "form-control" })
+    )
+    prompt = forms.CharField(
+        label='Prompt',
+        max_length=1500,
+        widget=forms.TextInput(attrs={ "class": "form-control" })
+    )
